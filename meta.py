20782ae347621c52eb22c5c28f4ee033b0e8abe6
Alex Knemeyer <alknemeyer@gmail.com>
2020-06-26 14:04:35 +0200

    Fixed bug in meta.py; it's still unused, though

--- meta.py
+++ meta.py
@@ -4,11 +4,11 @@ VALID_META = set((
     'spine', 'leg', 'tail',
     'front', 'back',
     'left', 'right',
+    'thigh', 'calf',
 ))
 
 def valid_meta(meta: Union[str, Iterable]):
-    if not isinstance(meta, str):
+    if isinstance(meta, str):
+        return meta in VALID_META
+    else:
         return all(valid_meta(m) for m in meta)
-    
-    if meta in VALID_META:
-        return True
