c9f406ec3b6c3fe4d3e82b5a0ee181227f560995
Alex Knemeyer <alknemeyer@gmail.com>
2020-08-26 11:37:40 +0200

    move matplotlib stub

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .monoped3D import monoped3D
 from .biped3D import biped3D
-from .quadruped3D import quadruped3D
+from .quadruped3D import quadruped3D, cheetah_params
 from . import tasks
 
-__all__ = ['monoped3D', 'biped3D', 'quadruped3D', 'tasks']
+__all__ = ['monoped3D', 'biped3D', 'quadruped3D', 'cheetah_params', 'tasks']
