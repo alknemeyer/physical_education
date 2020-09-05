8352df450465f243fc5dadbd5e1a5063961c78b9
Alex Knemeyer <alknemeyer@gmail.com>
2020-09-05 14:02:58 +0200

    Remove commented out code

--- stubs/pyomo/environ.pyi
+++ stubs/pyomo/environ.pyi
@@ -1,16 +1,6 @@
 # TODO: this is a work in progress!
 
 from typing import Any
-# from pyomo.core.base.PyomoModel import ConcreteModel
-# from pyomo.core.base.objective import Objective
-# from pyomo.core.base.param import Param
-# from pyomo.core.base.sets import Set
-# from pyomo.core.base.var import Var
-# from pyomo.core.base.constraint import Constraint, ConstraintList
-# from pyomo.core.base.rangeset import RangeSet
-# from pyomo.core.expr.logical_expr import inequality
-# from pyomo.core.expr.numvalue import value
-# from pyomo.core.expr.current import atan
 
 
 class ConcreteModel:
